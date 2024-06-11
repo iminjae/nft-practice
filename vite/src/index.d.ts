9ea4a88fca4b1179d99d4a5e3aea6f4238d1380e
iminjae <iminjae.github.com>
2024-06-11 10:08:34 +0900

    modify SaleNft

--- vite/src/index.d.ts
+++ vite/src/index.d.ts
@@ -1,14 +1,18 @@
-
 interface Window {
   ethereum: any;
 }
 
 interface NftMetadata {
-    name: string;
-    description: string;
-    image: string;
-    attributes?: {
-      trait_type: string;
-      value: string;
-    }[];
-  }
+  name: string;
+  description: string;
+  image: string;
+  attributes?: {
+    trait_type: string;
+    value: string;
+  }[];
+}
+
+interface SaleNftMetadata extends NftMetadata {
+  price: bigint;
+  tokenOwner: string;
+}
